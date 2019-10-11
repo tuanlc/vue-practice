74de26fe70ddd63e0bed9220be2cb3fa58a44aa9
Tuan Tuan LE <tlcong@linagora.com>
2019-10-11 17:32:23 +0700

    Conditional Rendering

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var app = new Vue({
   data: {
     product: 'Boots',
     image: './assets/vmSocks-green.jpg',
-    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+    inventory: 8,
+    isSale: true
   }
 });
