fe57d69fa5e61a3dfa1f8107383e32c9a5764a33
Tuan Tuan LE <tlcong@linagora.com>
2019-10-11 17:45:40 +0700

    List Rendering

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ var app = new Vue({
     image: './assets/vmSocks-green.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
     inventory: 8,
-    isSale: true
+    isSale: true,
+    details: ['80% cotton', '20% polyster', 'Gender-neutral'],
+    varians: [{
+      id: 123,
+      color: 'green'
+    }, {
+      id: 234,
+      color: 'blue'
+    }]
   }
 });
