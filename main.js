0be1893a22529439a14a34158305eeff1865a89b
Tuan Tuan LE <tlcong@linagora.com>
2019-10-14 13:25:48 +0700

    Computed Properties (Part 2)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var app = new Vue({
   data: {
     branch: 'Vue Mastery',
     product: 'Boots',
-    image: './assets/vmSocks-green.jpg',
+    selectedVariant: 0,
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
     inventory: 0,
     isSale: true,
@@ -11,26 +11,33 @@ var app = new Vue({
     varians: [{
       id: 123,
       color: 'green',
-      image: './assets/vmSocks-green.jpg'
+      image: './assets/vmSocks-green.jpg',
+      quantity: 10,
     }, {
       id: 234,
       color: 'blue',
-      image: './assets/vmSocks-blue.jpg'
+      image: './assets/vmSocks-blue.jpg',
+      quantity: 0
     }],
-    cart: 0,
-    inStock: false
+    cart: 0
   },
   methods: {
     onAddToCartClick: function() {
       this.cart++;
     },
-    changeColor: function(image) {
-      this.image = image;
+    updateProduct: function(index) {
+      this.selectedVariant = index;
     }
   },
   computed: {
     title() {
       return this.branch + ' ' + this.product;
+    },
+    image() {
+      return this.varians[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.varians[this.selectedVariant].quantity;
     }
   }
 });
